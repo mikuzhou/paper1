4129c1c063a84a8e465aba89a1dd5c41983daa5d
miku39 <1716406895@qq.com>
2023-11-07 02:10:07 -0600

    update

--- main.py
+++ main.py
@@ -52,12 +52,12 @@ def find_withdistractor():
     return util.score.cal_score(dlock_num, risk_num, find_dlock, find_risk);
 
 def main():
-    parser = argparse.ArgumentParser(description="Run specified phases of the grading process.")
-    parser.add_argument('--api_key', default=None,
-                        help="your api_key")
-    args = parser.parse_args()
-    if args.api_key:
-        openai_api_key = args.api_key
+    # parser = argparse.ArgumentParser(description="Run specified phases of the grading process.")
+    # parser.add_argument('--api_key', default=None,
+    #                     help="your api_key")
+    # args = parser.parse_args()
+    # if args.api_key:
+    #     openai_api_key = args.api_key
     file_name1 = "with.csv"
     file_name2 = "without.csv"
     file_name3 = "fancy.csv"
