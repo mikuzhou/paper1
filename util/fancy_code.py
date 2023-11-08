5fee7d02103dd741c4e27eda534f69dc3ec06b3a
miku39 <1716406895@qq.com>
2023-11-08 09:07:40 -0600

    update

--- util/fancy_code.py
+++ util/fancy_code.py
@@ -15,10 +15,10 @@ def pythonCodeGenerator(deadlock_amount,resource_contention_amount,non_contentio
         temperature=0,
     )
     system_template = """You are a Python code generator. Your task is to generate a Python code with five threads that includes {deadlock_amount} deadlock, {resource_contention_amount} thread resource contention, and {non_contentious_resources_amount} non-contentious resources. Notice that\
-    you need to hide these deadlocks and thread resource contentions, which means that those are hard to find. For example, you can put the code in a certain situation, and rename those deadlocks and resource contention without annotations."""
+    you need to hide these deadlocks and thread resource contentions, which means that those are hard to find. For example, you can put the code in a certain situation, and rename those deadlocks and resource contention without annotations. And you can increase the depth of the deadlocks"""
     system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
     human_template = """Please generate a Python code with five threads that includes {deadlock_amount} deadlock, {resource_contention_amount} thread resource contention, and {non_contentious_resources_amount} non-contentious resources.Notice that\
-    you need to hide these deadlocks and thread resource contentions, which means that those are hard to find. For example, you can put the code in a certain situation, and rename those deadlocks and resource contention without annotations."""
+    you need to hide these deadlocks and thread resource contentions, which means that those are hard to find. For example, you can put the code in a certain situation, and rename those deadlocks and resource contention without annotations. And you can increase the depth of the deadlocks"""
     human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
     chat_prompt = ChatPromptTemplate.from_messages(
         [system_message_prompt, human_message_prompt]
